f5cade3256cccc7900ea68dbf1177fe912ef8051
OHMz <159811790+kantaphob@users.noreply.github.com>
2024-03-25 14:52:56 +0700

    day 1

--- src/app/Layout/Components/header/header.component.ts
+++ src/app/Layout/Components/header/header.component.ts
@@ -3,6 +3,8 @@ import {select} from '@angular-redux/store';
 import {Observable} from 'rxjs';
 import { faEllipsisV } from '@fortawesome/free-solid-svg-icons';
 import {ThemeOptions} from '../../../theme-options';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-header',
@@ -12,7 +14,14 @@ export class HeaderComponent {
 
   faEllipsisV = faEllipsisV;
 
-  constructor(public globals: ThemeOptions) {
+  constructor(public globals: ThemeOptions, private httpClient: HttpClient) {
+    this.getData();
+  }
+  data: any = {};
+  getData(){
+    this.httpClient.get('http://localhost:8080/api/getData').subscribe(res => {
+      this.data = res;
+    });
   }
 
   @HostBinding('class.isActive')
